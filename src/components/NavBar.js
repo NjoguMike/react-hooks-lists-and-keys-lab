8d2d9c2082da0e4c66bc5b5fa4f01705742005a8
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-08-19 02:09:39 +0300

    Completed List and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const page = links.map(arr => <a href={"#"+arr} key={arr}>{arr}</a>)
+  return (
+    <nav>
+      {page}
+    </nav>
+    );
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,13 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  const project = projects.map(obj => <ProjectItem key={obj.id} name={obj.name} about={obj.about} technologies={obj.technologies}/>)
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{project}</div>
     </div>
   );
 }
